cd5f685c0d5eb880e457a00dc5ec7e6a602b81df
AleS4ndi <123819607+AleS4ndi@users.noreply.github.com>
2023-08-22 20:51:14 -0600

    Creación del tipo de ataque

--- AttackType.java
+++ AttackType.java
@@ -1,6 +1,16 @@
 package pokemon;
 
 public enum AttackType {
-  PHYSIC_ATTACK,
-  SPECIAL_ATTACK
+  PHYSIC_ATTACK(1),
+  SPECIAL_ATTACK(2);
+
+  private final int value;
+
+  AttackType(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
 }
